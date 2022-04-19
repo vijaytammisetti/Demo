cfd6ec4268e135263123604194507c5f1f3de43a
vijay tammisetti <47395843+vijaytammisetti@users.noreply.github.com>
2022-04-19 10:35:09 +0530

    Update UserController.java

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,1 +1,12 @@
-vfxvdsxvcsavva
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping
+    public String demo() {
+        return "hellow";
+    }
+}
